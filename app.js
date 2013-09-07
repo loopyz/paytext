3fe4faefd746960ee1263c3c1567ef87114ef52e
Niveditha Jayasekar <jnive05@gmail.com>
2013-09-07 14:55:28 -0400

    only redirect some pages if not logged in

--- app.js
+++ app.js
@@ -7,20 +7,9 @@ var express = require('express'),
 var app = express();
 
 var CONNECTION = null;
-// non-encrypted passwords :D
-var USERS_PASSWORD = {
-  lucy: 'pwd',
-  nive: 'pwd',
-  alexis: 'pwd',
-};
 
-var SELLER_PHONE = '4087181204';
-
-var ITEMS_COST = {
-  123: 0.01,
-  456: 0.01,
-  789: 0.01,
-}
+//anyone can access the following pages
+var ALL_ACCESS = ['/', '/login', '/signup', '/purchase', '/make_purchase'];
 
 var generate_qr = function (id) {
     var link = 'paytext.herokuapp.com/purchase/' + id;
@@ -41,8 +30,7 @@ app.use(express.static('static/'));
 // use this stupid middleware to check if the user is logged before loading
 // each page, if he's not ask him to log in.
 app.use(function(req, res, next) {
-  if (req.path!=='/login' && req.path!=='/' && req.path!='/signup' &&
-      !req.cookies.user) {
+  if (ALL_ACCESS.indexOf(req.path)===-1 && !req.cookies.user) {
     res.redirect('/');
   } else {
     next();
